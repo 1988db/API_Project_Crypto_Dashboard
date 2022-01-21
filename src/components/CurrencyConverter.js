05d9f04e5ee77d0cd8af833068d56b105730b7a1
1988db <elozrana@gmail.com>
2022-01-21 21:50:25 +0100

    added currencies to the form

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,9 +1,59 @@
+import { useState} from 'react';
 import ExchangeRate from './ExchangeRate';
 
 const CurrencyConverter = () => {
+  const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA'];
+  const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC');
+  const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('BTC');
+  console.log(chosenSecondaryCurrency)
+
     return (
       <div className="currency-converter">
-        Currency Converter
+        <h2>Currency Converter</h2>
+        <div className="input-box">
+          <table>
+          <tbody>
+            <tr>
+              <td>Primary currency:</td>
+              <td>
+                <input
+                  type="number"
+                  name="currency-amount-1"
+                  value = {""}>
+                </input>
+              </td>
+              <td>
+                <select
+                  value={chosenPrimaryCurrency}
+                  name="currency-option-1"
+                  className="currency-options"
+                  onChange={(e) => setChosenPrimaryCurrency(e.target.value)}>
+                    {currencies.map((currency, _index) => (<option key={_index}>{currency}</option>))}
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td>Secondary currency:</td>
+              <td>
+                <input
+                  type="number"
+                  name="currency-amount-2"
+                  value = {""}>
+                </input>
+              </td>
+              <td>
+                <select
+                  value={chosenSecondaryCurrency}
+                  name="currency-option-2"
+                  className="currency-options"
+                  onChange={(e) => setChosenSecondaryCurrency(e.target.value)}>
+                    {currencies.map((currency, _index) => (<option key={_index}>{currency}</option>))}
+                </select>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        </div>        
         <ExchangeRate/>
       </div>
     );
