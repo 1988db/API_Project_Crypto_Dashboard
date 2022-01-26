dd4f08aa8b40f8b321f2b439dcd0e5e109e39aff
1988db <elozrana@gmail.com>
2022-01-26 23:32:56 +0100

    articles titles appear in the news feed

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,7 +1,34 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
 const NewsFeed = () => {
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {    
+    const options = {
+      method: 'GET',
+      url: 'https://crypto-news-live3.p.rapidapi.com/news',
+      headers: {
+        'x-rapidapi-host': 'crypto-news-live3.p.rapidapi.com',
+        'x-rapidapi-key': 'd25c95f5f2msh2f5f454ce3aaac5p15efb4jsn3ae259e71d27'
+        }
+      };
+
+      axios.request(options).then(function (response) {      	
+        setArticles(response.data);
+
+      }).catch(function (error) {
+	      console.error(error);
+      });     
+    }, [])
+
+    console.log(articles);
+    const first7articles = articles?.slice(0,7);
+
     return (
       <div className="news-feed">
-        NewsFeed
+        <h2>NewsFeed</h2>
+        {first7articles?.map((article, _index) => (<p key={_index}>{article.title}</p>) )}
       </div>
     );
   }
